a9fbd2774679fb1b3f02732e98b727ce62ffa8e5
Nicole Barnhouse <nicolebarnhouse@gmail.com>
2023-12-13 15:10:52 -0600

    still reworking the total

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -2,25 +2,22 @@
 let currentIndex = 1;
 let totalMonthly = 0;
 
-// Create an empty array for temp data
-const totalSalary = [];
-
-
 //Form submission function
 function addEmp(event) {
     event.preventDefault();
     //console.log('test');
 
-
-    // Add The employee Data to the page
-    let employeeTable = document.querySelector('#employeeData');
+    // Add form data to the page. 
+    var form = document.getElementById("myForm");
+    //Variables for the form
     let firstVal = document.querySelector('#firstNameInput').value;
     let lastVal = document.querySelector('#lastNameInput').value;
     let idVal = document.querySelector('#idInput').value;
     let titleVal = document.querySelector('#titleInput').value;
     let salaryVal = document.querySelector('#annualSalaryInput').value;
-    let totalIncomeMonthly = document.querySelector('#total_monthly').value;
-    var form = document.getElementById("myForm");
+
+        // Add The employee Data to the page
+        let employeeTable = document.querySelector('#employeeData');
 
        employeeTable.innerHTML += `
             <tr>
@@ -37,9 +34,7 @@ function addEmp(event) {
         console.log(`Current Index: ${currentIndex}`)
         form.reset();
 
-        //take total salary from first index divide by 12 and return
-       //as next salary at new index is added, add two togther and return
-       //loop through data in table via index
+        
 
 }
 
